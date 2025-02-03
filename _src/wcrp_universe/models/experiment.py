89e2042fb553315b51722fcf87d1816c079450ff
Sébastien Gardoll <sebastien@gardoll.fr>
2025-02-03 11:58:51 +0100

    refactoring

--- _src/wcrp_universe/models/experiment.py
+++ _src/wcrp_universe/models/experiment.py
@@ -1,77 +1,25 @@
-
-from __future__ import annotations 
-from datetime import (
-    datetime,
-    date
-)
-from decimal import Decimal 
-from enum import Enum 
-import re
-import sys
-from typing import (
-    Any,
-    ClassVar,
-    List,
-    Literal,
-    Dict,
-    Optional,
-    Union
-)
-from wcrp_universe.models.activity import Activity
-from pydantic.version import VERSION  as PYDANTIC_VERSION 
-if int(PYDANTIC_VERSION[0])>=2:
-    from pydantic import (
-        BaseModel,
-        ConfigDict,
-        Field,
-        RootModel,
-        field_validator
-    )
-else:
-    from pydantic import (
-        BaseModel,
-        Field,
-        validator
-    )
-
-metamodel_version = "None"
-version = "None"
+from pydantic import Field
+from esgvoc.api.data_descriptors.data_descriptor import DrsPlainTermDataDescriptor
 
 
-class ConfiguredBaseModel(BaseModel):
-    model_config = ConfigDict(
-        validate_assignment = True,
-        validate_default = True,
-        extra = "allow",
-        arbitrary_types_allowed = True,
-        use_enum_values = True,
-        strict = False,
-    )
-    pass
-
-
-class Experiment(ConfiguredBaseModel):
+class Experiment(DrsPlainTermDataDescriptor):
     """
-    an 'experiment' refers to a specific, controlled simulation conducted using climate models to investigate particular aspects of the Earth's climate system. These experiments are designed with set parameters, such as initial conditions, external forcings (like greenhouse gas concentrations or solar radiation), and duration, to explore and understand climate behavior under various scenarios and conditions.
+    An 'experiment' refers to a specific, controlled simulation conducted using climate models to \
+    investigate particular aspects of the Earth's climate system. These experiments are designed \
+    with set parameters, such as initial conditions, external forcings (like greenhouse gas \
+    concentrations or solar radiation), and duration, to explore and understand climate behavior \
+    under various scenarios and conditions.
     """
-
-    id: str 
-    validation_method: str = Field(default ="list")
-    activity: List[str] = Field(default_factory=list)
+    activity: list[str] = Field(default_factory=list)
     description: str 
-    tier: Optional[int] 
+    tier: int|None
     experiment_id: str 
-    sub_experiment_id: Optional[List[str]] 
-    experiment: str 
-    required_model_components: Optional[List[str]] 
-    additionnal_allowed_model_components: Optional[List[str]] = Field(default_factory=list) 
-    start_year: Optional[int] 
-    end_year: Optional[int] 
-    min_number_yrs_per_sim: Optional[int] 
-    parent_activity_id: Optional[List[str]] 
-    parent_experiment_id: Optional[List[str]] 
-
-
-# Model rebuild
-# see https://pydantic-docs.helpmanual.io/usage/models/#rebuilding-a-model
-Experiment.model_rebuild()
+    sub_experiment_id: list[str]|None
+    experiment: str
+    required_model_components: list[str]|None
+    additional_allowed_model_components: list[str] = Field(default_factory=list) 
+    start_year: int|None
+    end_year: int|None
+    min_number_yrs_per_sim: int|None
+    parent_activity_id: list[str]|None
+    parent_experiment_id: list[str]|None
--- _src/wcrp_universe/models/mip_era.py
+++ _src/wcrp_universe/models/mip_era.py
@@ -1,70 +1,8 @@
+from esgvoc.api.data_descriptors.data_descriptor import DrsPlainTermDataDescriptor
 
 
-from __future__ import annotations 
-from datetime import (
-    datetime,
-    date
-)
-from decimal import Decimal 
-from enum import Enum 
-import re
-import sys
-from typing import (
-    Any,
-    ClassVar,
-    List,
-    Literal,
-    Dict,
-    Optional,
-    Union
-)
-from idna import intranges_contain
-from pydantic.version import VERSION  as PYDANTIC_VERSION 
-if int(PYDANTIC_VERSION[0])>=2:
-    from pydantic import (
-        BaseModel,
-        ConfigDict,
-        Field,
-        RootModel,
-        field_validator
-    )
-else:
-    from pydantic import (
-        BaseModel,
-        Field,
-        validator
-    )
-
-metamodel_version = "None"
-version = "None"
-
-
-class ConfiguredBaseModel(BaseModel):
-    model_config = ConfigDict(
-        validate_assignment = True,
-        validate_default = True,
-        extra = "allow",
-        arbitrary_types_allowed = True,
-        use_enum_values = True,
-        strict = False,
-    )
-    pass
-
-
-
-
-
-class MipEra(ConfiguredBaseModel):
-
-
-
-    id: str 
-    start : int
-    end : int
-    name : str 
-    type : str 
-    url : str
-    drs_name : str
-# Model rebuild
-# see https://pydantic-docs.helpmanual.io/usage/models/#rebuilding-a-model
-MipEra.model_rebuild()
+class MipEra(DrsPlainTermDataDescriptor):
+    start: int
+    end: int
+    name: str 
+    url: str

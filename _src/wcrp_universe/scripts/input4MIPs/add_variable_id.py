f2c4a480371032fec5cd8d019ef34f3a4771bc4c
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2025-06-16 10:53:44 +0200

    chore : cleaning, removing file if not necessary

--- _src/wcrp_universe/scripts/input4MIPs/add_variable_id.py
+++ _src/wcrp_universe/scripts/input4MIPs/add_variable_id.py
@@ -20,16 +20,17 @@ def create_variable_json_files(variable_ids, output_dir):
     """Create individual JSON files for each variable_id."""
     os.makedirs(output_dir, exist_ok=True)
 
-    for var_id in variable_ids:
+    for validatation_key in variable_ids:
+        var_id = validatation_key.lower()
         json_content = {
             "@context": "000_context.jsonld",
             "id": var_id,
-            "cmip_acronym": var_id,
+            "cmip_acronym": validatation_key,
             "long_name": "",
             "standard_name": "",
             "type": "variable",
             "units": "",
-            "drs_name": var_id,
+            "drs_name": validatation_key,
         }
 
         filename = f"{var_id}.json"
@@ -41,6 +42,18 @@ def create_variable_json_files(variable_ids, output_dir):
         print(f"Created: {filepath}")
 
 
+def remove_file(file_path):
+    """Removes the file at the given file_path if it exists."""
+    try:
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            print(f"File '{file_path}' has been removed.")
+        else:
+            print(f"File '{file_path}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred while deleting the file: {e}")
+
+
 if __name__ == "__main__":
     json_file = "_src/wcrp_universe/scripts/input4MIPs/original_CVs/input4MIPs_db_file_entries.json"
     output_directory = "variable"
@@ -48,8 +61,8 @@ if __name__ == "__main__":
     unique_ids = extract_unique_variable_ids(json_file)
 
     print(f"Found {len(unique_ids)} unique variable_id values:")
-    for var_id in unique_ids:
+    for var_id in unique_ids:  # here it is not ids, it is validation-key
         print(var_id)
-
+        remove_file(f"variable/{var_id}.json")
     print(f"\nCreating JSON files in {output_directory}/ directory...")
     create_variable_json_files(unique_ids, output_directory)

8fc88e3279a7f8863b9d9f3ad20c30db903f1eb8
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2025-10-27 10:45:32 +0000

    Update docstrings

--- scripts/create-new-version.py
+++ scripts/create-new-version.py
@@ -7,21 +7,21 @@ def set_arg_parser():
     """
     Creates an argument parser to take the submitted issue body as an argument.
 
-        :returns: Argument parser
+        :returns: Argument parser.
     """
-    parser = argparse.ArgumentParser(description="Version variable")
-    parser.add_argument("number", help="The current release version")
-    parser.add_argument("changes", help="Changes made on the previous commmit")
+    parser = argparse.ArgumentParser(description="Version variable.")
+    parser.add_argument("number", help="The current release version.")
+    parser.add_argument("changes", help="Changes made on the previous commmit.")
     args = parser.parse_args()
   
     return args
 
 def split_version_components(number):
     """
-    Splits the input version string into 3 integers
+    Splits the input version string into 3 integers.
 
-        :param number: The numerical part of the current version
-        :returns: Each element of the current version as individual integers
+        :param number: The numerical part of the current version.
+        :returns: Each element of the current version as individual integers.
     """
     version = number.split(".")
     major = int(version[0])
@@ -33,10 +33,10 @@ def split_version_components(number):
 
 def determine_update_type(changes): 
     """
-    Determines the type of release update that should be triggered based off of the previous commit
+    Determines the type of release update that should be triggered based off of the previous commit.
 
         :param changes: A list of changes made in the previous commit.
-        :returns: The type of release update that should be triggered (patch, major, minor, undetermined)
+        :returns: The type of release update that should be triggered (patch, major, minor, undetermined).
     """
     changes = re.split(" |\n", changes)
     minor_conditions = ["A", "M", "D"]   #edit me later
@@ -54,11 +54,11 @@ def determine_update_type(changes):
 
 def update_version(number, changes):
     """
-    Updates each element of the current version based on the update type
+    Updates each element of the current version based on the update type.
 
-        :param number: The numerical part of the current version
-        :param update_type: major, minor or patch type release
-        :returns: The new, complete numerical element of the version as a string
+        :param number: The numerical part of the current version.
+        :param update_type: major, minor or patch type release.
+        :returns: The new, complete numerical element of the version as a string.
     """
     major, minor, patch = split_version_components(number)
     update_type = determine_update_type(changes)
@@ -72,10 +72,10 @@ def update_version(number, changes):
     elif update_type == "patch":
         patch += 1
     elif update_type == "undetermined":
-        print("WARNING: Merge conflict tor uncategorised change reported")
+        print("WARNING: Merge conflict or uncategorised change reported.")
         sys.exit(1)
     else:
-        print ("WARNING: update type not recognised")
+        print ("WARNING: update type not recognised.")
         sys.exit(1)
 
     new_version = f"v{major}.{minor}.{patch}"
@@ -86,19 +86,11 @@ def main():
     """
     Holds the main body of the script.
 
-        :returns: None
+        :returns: None.
     """
     args = set_arg_parser()
-    number = args.number
-    changes = args.changes
-
-    print(f"The output of number is: {number}\nThe output of changes is: {changes}")
   
-    new_version = update_version(number, changes)
+    new_version = update_version(args.number, args.changes)
 
     with open(os.environ["GITHUB_OUTPUT"], "a") as out:
         out.write(f"version={new_version}\n")
-
-if __name__ == "__main__":
-    main()
-

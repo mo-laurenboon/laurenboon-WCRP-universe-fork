783fe8f7920b58d89b79cf78f03696f98732e615
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2025-10-29 15:37:08 +0000

    expanded structure added

--- scripts/extract-from-jsonlds.py
+++ scripts/extract-from-jsonlds.py
@@ -3,45 +3,61 @@ This scripts uses both pyld and RDFlib to extract tuples from JSON-LD files with
 """
 
 from pathlib import Path
+from rdflib import Graph
 from pyld import jsonld
 import json
-from rdflib import Graph
 
-def load_jsonld_files(input_dir):
+def get_jsonld_files(input_dir):
     """
     Load all JSON-LD files from the provided input directory.
 
         :param input_dir: The directory where the JSON-LD are stored.
-        :returns: The paths of the files and the files themselves as a list.
+        :returns: The paths of the files as a list.
         :raises FileNotFoundError: An error is printed if no JSON-LD files are found in input_dir.
     """
-    print("Files loading....")
+    print("Files loading............")
     paths = sorted(input_dir.glob("*.jsonld"))
     if not paths:
         raise FileNotFoundError(f"No JSON-LD files found in {input_dir.resolve()}")
         sys.exit(1)
     for path in paths:
         print(f"- {path.name}")
-    files = [json.loads(path.read_text(encoding="utf-8")) for path in paths]
 
-    return paths, files
-  
-#extract using rdflib
-def main():
-    """
-    Holds the main body of the script
-    """
-    input_dir = Path("JSONLDs")
-    paths, files = load_jsonld_files(Path("JSONLDs"))
-    
+    return paths
+
+
+def rdflib_extraction(paths)
     g = Graph()
     for path in paths:
         print(f"Extracting information from {path}............")
         g.parse(path, format="json-ld")
         for s, p, o in g:
             print(f"Subject: {s}\nPredicate: {p}\nObject: {o}")
+
+
+def get_expanded_structure(paths):
+    expansions=[]
+    for path in paths:
+        print(f"Getting expanded structure for {path}............")
+        with open(path) as f:
+            file = json.load(f)
+        expanded = jsonld.expand(file)
+        print("Expanded version of file is...")
+        print(json.dumps(expanded, indent=4))
+        expansions.append(expanded)
         
+    return expansions
     
 
+def main():
+    """
+    Holds the main body of the script
+    """
+    input_dir = Path("JSONLDs")
+    paths = load_jsonld_files(Path("JSONLDs"))
+    rdflib_extraction(paths)
+    expansions = get_expanded_structure(paths)
+
+        
 if __name__ == "__main__":
     main()
